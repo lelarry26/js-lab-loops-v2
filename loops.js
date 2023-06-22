a5672a92188698042fdcb026b11a97a29a66bf51
Larry Le <larrygsu@gmail.com>
2023-06-22 10:39:20 -0400

    Updating with completed exercises

--- loops.js
+++ loops.js
@@ -52,3 +52,107 @@ const prompt = require('prompt-sync')({sigint: true});
 //     if (i % 2 == 0)
 //     console.log (i)
 // }
+
+//7. Write a `for loop` to print the numbers 0 - 20
+// for (let i=0; i < 20; i++){
+
+//   console.log(i)
+// }
+
+//8. Write a `while loop` to print the numbers 0 - 20
+// let count = -1
+// while (count < 20){
+//   count = count + 1
+//   console.log (count)
+// }
+
+//9. Write a `for loop` that prints the numbers from 10 to 1 to the console.
+// for (let i=10; i > 0; i--){
+//   console.log (i)
+// }
+
+//10. Write a `while loop` that prints the numbers from 10 to 1 to the console.
+// let count = 11
+// while (count > 1) {
+//   count = count - 1
+//   console.log (count)
+// }
+
+//11. Write a `while loop` that keeps asking a user to enter in a word.  The program loop should stop when the user enters the workd `exit`
+// let ask = prompt ("Enter in a word") 
+// while ( ask !== "exit"){
+// ask = prompt ("Enter in a word")
+// console.log (ask)
+// }
+//Is this correct?
+
+//12. Print only the ODD values from 3 - 29, one number per line
+//for loop variant-----------------------------------------------
+// for (let i=3; i <= 29; i++){
+//   if (i % 2 !== 0)
+//   console.log(i)
+// }
+//Is there a better way?
+//while loop variant---------------------------------------------
+// let i = 1
+// while (i < 29) {
+//   i = i + 1
+//     if (i % 2 !== 0)
+//     console.log(i)
+// }
+//Is there a better way?
+
+//13. Print the EVEN numbers 12 down to -14 in descending order, one number per line
+//for loop variant------------------------------------------------
+// for (let i=12; i >= -14; i--){
+//   if (i % 2 == 0)
+//   console.log(i)
+// }
+//Is there a better way?
+//while loop variant---------------------------------------------
+// let i = 13
+// while (i  > -14) {
+//   i = i - 1
+//     if (i % 2 == 0)
+//       console.log(i)
+// }
+//Is there a better way?
+
+//14. Print the numbers 50 down to 20 in descending order, but only if the numbers are multiples of 3
+//for loop variant------------------------------------------------
+// for (let i = 50; i > 20; i--){
+//   if (i % 3 ==0)
+//   console.log(i)
+// }
+//Is there a better way?
+//while loop variant---------------------------------------------
+// let i = 50
+// while (i >= 20){
+//   i = i - 1
+//   if (i % 3 == 0)
+//   console.log(i)
+// }
+//Is there a better way?
+
+//15. Write a loop that calculates the sum of the numbers from 1 to 100 and prints the result 
+//to the console.
+//for loop variant------------------------------------------------
+// total = 0
+// for (let i = 1; i < 101; i++)
+//   if (i !== 101){
+//     total = (total + i)
+// console.log (total)
+// }
+//How do I get to the result without the other values printed?
+//while loop variant---------------------------------------------
+// let i = 0
+// total =  0
+//   while (i <= 100){
+//     i = i + 1
+//     if (i !== 101)
+//       total = (total + i)
+//       console.log (total)
+//   }
+//How do I get to the result without the other values printed?
+
+
