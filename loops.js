2039eae7578e01f0229054f5f047f6af631e56b3
Larry Le <larrygsu@gmail.com>
2023-06-21 23:19:41 -0400

    Updating with completed exercises up to 6

--- loops.js
+++ loops.js
@@ -6,5 +6,49 @@ const prompt = require('prompt-sync')({sigint: true});
   const name = prompt('What is your name?');
   console.log(`Hey there ${name}`);
 
-  All user input will be read as a String, so in order to treat user input as numbers, you’ll need to convert the input:
+  All user input will be read as a String, so in order to treat user input as numbers, 
+  you’ll need to convert the input:
 */
+
+
+//1. Repeat the following phrase 5 times to your terminal by using a `for loop`.
+//"Repetition is a good thing"
+
+// for (let i=0; i < 5; i++) {
+//   console.log("Repetition is a good thing")
+// }
+
+//2. Solve the problem above again using a `while loop`.
+// let count = 0;
+
+// while (count < 5){
+//   count = count + 1;
+//   console.log("Repetition is a good thing");
+// }
+
+//3. Write a `for loop` that prints the numbers from 1 to 10 to the console.
+// for (let i=1; i < 11; i++){
+
+//   console.log(i)
+// }
+
+//4. Write a `while loop` that prints the numbers from 1 to 10 to the console.
+// let count = 0
+// while (count < 10){
+//   count = count + 1;
+//   console.log (count)
+// }
+
+//5. Write a `for loop` that prints the even numbers from 1 to 20 to the console.
+// for (let i=1; i < 21; i++){
+//   if (i % 2 === 0)
+//     console.log(i)
+// }
+
+//6. Write a `while loop` that prints the even numbers from 1 to 20 to the console.
+// let i = 0
+// while (i < 20){
+//   i = i + 1
+//     if (i % 2 == 0)
+//     console.log (i)
+// }
